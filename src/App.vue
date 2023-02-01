1dc184d1ad59c98d7ddd39002a7b7c1fba532241
Roberto Larivera <robylariv@gmail.com>
2023-02-01 19:22:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+
+export default {
+  components: {
+    AppTitle,
+    AppHero,
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<template>
+
+  <AppTitle/>
+  <AppHero/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            src: 'https://picsum.photos/200/300'
+        }
+    }
+}
+</script>
+
+<template>
+    <img :src="src">
+</template>
+
+<style scoped>
+
+</style>
